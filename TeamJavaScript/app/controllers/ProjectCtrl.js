72e5f40c8ca1fabb1db2fe9e7579ce201d2c8b57
Anderson Dorow <andersondorow@gmail.com>
2015-12-08 12:31:58 +0100

    listing topics from project

--- TeamJavaScript/app/controllers/ProjectCtrl.js
+++ TeamJavaScript/app/controllers/ProjectCtrl.js
@@ -1,6 +1,16 @@
 angular.module('myApp')
     .controller('ProjectCtrl', function($scope, $http, $cookies, $routeParams, $location, BaseUrl) {
 
-        $scope.params = $routeParams;
+        $scope.project = $routeParams;
+
+        $http.defaults.headers.common.Authorization = 'Bearer ' + $cookies.getObject('token').access_token;
+
+        $http.get(BaseUrl + '/bcf/1.0/projects/' + $routeParams.project_id + '/topics')
+        .success(function(data) {
+            $scope.topics = data;
+        })
+        .error(function(data) {
+            $scope.error = data;
+        });
 
     });
