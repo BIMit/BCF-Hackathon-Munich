61f039fe1c6a07b59c2763b9800c9cdc967d8cc3
Georg Dangl <dangl@iabi.eu>
2015-12-08 09:54:05 +0100

    Got the authentication=)

--- Team JavaScript/app/app.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
